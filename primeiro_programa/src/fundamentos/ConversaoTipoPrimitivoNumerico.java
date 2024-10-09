d3f2bde4b57d889da91dcf77715c9ca1f8489fe4
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-10-08 23:29:13 -0300

    Add files via upload

---
+++ primeiro_programa/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+    public static void main(String[] args) {
+
+        double a = 1;
+        System.out.println(a);
+
+        float b = (float) 1.0F;
+    }
+}

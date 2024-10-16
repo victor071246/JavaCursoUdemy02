71b4697e5dbf78d9a8ceec6a56fc565d3ab5a770
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-10-15 23:32:33 -0300

    Add files via upload

--- classe/Data.java
+++ classe/Data.java
@@ -5,12 +5,31 @@ public class Data {
     int dia;
     int mes;
     int ano;
+    
 
-    String obterDataFormatada(){
-        return String.format("%d/%d/%d", dia, mes, ano);
+    Data() {
+        // dia = 1;
+        // mes = 1;
+        // ano = 1970;
+        this(1, 1, 1970);
     }
 
-    void imprimirDataFormatada(){
-        System.out.println(obterDataFormatada());
+
+    Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
+
+
+    String obterDataFormatada() {
+        final String formato = "%d/%d/%d";
+        return String.format(formato, dia, mes, ano);
+    }
+
+    void imprimirDataFormatada() {
+        System.out.println(this.obterDataFormatada());
+    }
+
+
 }
